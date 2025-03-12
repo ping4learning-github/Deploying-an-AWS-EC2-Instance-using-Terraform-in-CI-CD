c4542b4ccfbc2f2c9b5dc129c734289876bb595f
Harshvardhan <ping4learning@gmail.com>
2025-03-12 11:59:08 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,6 +1,6 @@
 # VPC Creation
 resource "aws_vpc" "my_vpc" {
-  cidr_block           = "10.0.0.0/24"
+  cidr_block           = "10.0.0.0/23"
   enable_dns_support   = true
   enable_dns_hostnames = true
 
