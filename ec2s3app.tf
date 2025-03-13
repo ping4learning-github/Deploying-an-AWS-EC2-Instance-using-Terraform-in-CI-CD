daa64009fc1406c96f7871d28fb03e9b19c18b8b
Harshvardhan <ping4learning@gmail.com>
2025-03-13 11:25:25 +0530

    Update ec2s3app.tf

--- ec2s3app.tf
+++ ec2s3app.tf
@@ -8,7 +8,7 @@ resource "aws_instance" "myapp_ec2" {
   associate_public_ip_address = true
 
   root_block_device {
-    volume_size           = 10
+    volume_size           = 20
     volume_type           = "gp3"
     encrypted             = true
     delete_on_termination = true
